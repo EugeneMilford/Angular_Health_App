65e7a85bf48a73c66dfd1e94af7561cc3851fb8e
Eugene Milford <gmilford321@gmail.com>
2023-11-20 22:12:26 +0200

    Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FoodsComponent } from './foods/foods.component';
+import { ExerciseComponent } from './exercise/exercise.component';
+import { SleepComponent } from './sleep/sleep.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FoodsComponent,
+    ExerciseComponent,
+    SleepComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
